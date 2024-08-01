ba10131211afbcdfa263c313211ca624e712d4c2
Unnic-web <korzikov1sasa@gmail.com>
2024-08-02 01:24:24 +0500

    Add files via upload

---
+++ Clearing-deleted-accounts.py
@@ -0,0 +1,52 @@
+__version__ = (1, 0, 0)
+# meta developer: @unnic
+import asyncio
+from telethon import types
+from telethon.errors import ChatAdminRequiredError
+from .. import loader, utils
+
+@loader.tds
+class DelMod(loader.Module):
+    """Module for Cleaning Up Deleted Accounts in Chat"""
+
+    strings = {
+        "name": "Del",
+        "author": "@HikkTutor",
+    }
+
+    async def client_ready(self, client, db):
+        self._client = client
+        self.db = db
+
+    @loader.command()
+    async def delete(self, message: types.Message):
+        """Удаляет удалённые аккаунты из чата"""
+        chat = await message.get_chat()
+
+        if isinstance(chat, types.User):
+            await utils.answer(message, "<b><emoji document_id=5787313834012184077>😀</emoji> Эта команда предназначена только для групп</b>")
+            return
+
+        if not chat.admin_rights and not chat.creator:
+            await utils.answer(message, "<emoji document_id=5787544344906959608>ℹ️</emoji> <b>Недостаточно прав для выполнения этой команды.</b>")
+            return
+
+        removed_count = 0
+        
+        edit_message = await message.edit("<b><emoji document_id=5188311512791393083>🔎</emoji> Поиск удалённых аккаунтов</b>")
+
+        async for user in self._client.iter_participants(chat):
+            if user.deleted:
+                try:
+                    await self._client.kick_participant(chat, user)
+                    removed_count += 1
+                except ChatAdminRequiredError:
+                    await utils.answer(message, "<emoji document_id=5787544344906959608>ℹ️</emoji> <b>Для выполнения команды необходимы права администратора</b>")
+                    return
+                except Exception as e:
+                    await utils.answer(message, f"<emoji document_id=5787544344906959608>ℹ️</emoji> <b>Ошибка при удалении аккаунта {user.id}: {str(e)}</b>")
+
+        if removed_count == 0:
+            await edit_message.edit("<emoji document_id=5341509066344637610>😎</emoji> <b>Здесь нет ни одного удалённого аккаунта</b>")
+        else:
+            await edit_message.edit(f"**<emoji document_id=5328302454226298081>🫥</emoji> <b>Удалено {removed_count} удалённых аккаунтов</b>")
