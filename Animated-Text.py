d0c0d4b76f40c0ab60aa525138612377aff39017
Alexander Alexandrovich <korzikov1sasa@gmail.com>
2024-08-18 14:12:06 +0300

    Update Animated-Text.py

--- Animated-Text.py
+++ Animated-Text.py
@@ -138,5 +138,5 @@ class AnimatedTextMod(loader.Module):
         "<emoji document_id=5818865088970362886>❕</emoji> <b>В этих функционалах нету, знаков ?!+-= и так делее.\n"
         "Это не потому что я не смог добавить, а потому что в некоторых паках их просто нету</b>\n\n"
         "<emoji document_id=5875452644599795072>🔞</emoji> <b>Разработчик: @unnic</b>\n"
-    )
-    await message.edit(instruction, parse_mode='html')
+    )   
+        await message.edit(instruction, parse_mode='html')
